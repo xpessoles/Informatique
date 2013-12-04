4ceff385b24b488c06d197b477aa399c16d2155b
xpessoles <xpessoles.ptsi@free.fr>
2013-12-04 23:58:32 +0100

    MAJ en cours sur l'invariance

--- CI_02_AlgorithmiqueProgrammation/03_Invariance/programme/programme.py
+++ CI_02_AlgorithmiqueProgrammation/03_Invariance/programme/programme.py
@@ -7,4 +7,29 @@ def division(a,b):
     print("reste ", reste)
     print("quotient ", quotient)
 
-division(6,10)
+
+
+def fonction(n):
+    i=0
+    res=1
+    while i<n:
+        res = res*2
+        i=i+1
+    return res
+
+def fonction2(n):
+    i=n
+    res=1
+    while i!=0:
+        res = res*2
+        i=i-1
+    return res
+
+print(fonction(0))
+print(fonction2(0))
+print(fonction(1))
+print(fonction2(1))
+print(fonction(2))
+print(fonction2(2))
+print(fonction(3))
+print(fonction2(3))
