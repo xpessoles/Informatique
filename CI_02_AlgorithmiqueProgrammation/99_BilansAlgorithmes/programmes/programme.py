9c1bbcad47bcf2f532acf9e8a07b30b14aca51ae
Xavier Pessoles <xpessoles.ptsi@free.fr>
2014-12-22 17:55:38 +0100

    MAJ

--- CI_02_AlgorithmiqueProgrammation/99_BilansAlgorithmes/programmes/programme.py
+++ CI_02_AlgorithmiqueProgrammation/99_BilansAlgorithmes/programmes/programme.py
@@ -201,13 +201,95 @@ def integrale_rectangles_milieu(f,a,b,nb):
     return res
 
 # Exemple :    
-def ff(x):
-    return 2#x*x       
-print(integrale_rectangles_gauche(ff,0,1,10000))
-print(integrale_rectangles_droite(ff,0,1,10000))
-print(integrale_rectangles_milieu(ff,0,1,5000))
+#def ff(x):
+#    return 2#x*x       
+#print(integrale_rectangles_gauche(ff,0,1,10000))
+#print(integrale_rectangles_droite(ff,0,1,10000))
+#print(integrale_rectangles_milieu(ff,0,1,5000))
+#print(1/3)
 
-print(1/3)
+def euler_explicite(tau,y0,yf,tf,nb):
+    """
+    Résolution d'une équation différentielle d'ordre 1 en utilisant la méthode
+    d'Euler explicite.
+    Keywords arguments :
+    tau -- flt, constante de temps de l'équation différentielle 
+    y0 -- flt, valeur initiale de y(t)
+    yf -- flt valeur finale de y(t)
+    tf -- flt temps de fin de la simulation
+    nb -- int, nombre d'échantillons pour la simulation
+    """
+    t = 0
+    y = y0 
+    pas = tf / nb
+    res = []
+    while t < tf:
+        res.append((t,y))
+        y = y + pas*(yf-y)/tau
+        t = t + pas
+    return res
+
+#res = euler_explicite(1,0,5,25,1000)
+#x,y = [],[]
+#for i in range(len(res)):
+#    x.append(res[i][0])
+#    y.append(res[i][1])
+#plot(x,y)
+
+
+def recherche_pivot(A,i):
+    n = len(A) # le nombre de lignes
+    j = i # la ligne du maximum provisoire
+    for k in range(i+1, n):
+        if abs(A[k][i]) > abs(A[j][i]):
+            j = k # un nouveau maximum provisoire
+    return j
+    
+def echange_lignes(A,i,j):
+    # Li <-->Lj
+    A[i][:],A[j][:]=A[j][:],A[i][:]
+
+
+def transvection_ligne(A, i, j, mu):
+    # L_i <- L_i + mu.L_j """
+    nc = len(A[0]) # le nombre de colonnes
+    for k in range(nc):
+        A[i][k] = A[i][k] + mu * A[j][k]
+
+def resolution_gauss_jordan(AA, BB):
+    """Résolution de AA.X=BB; AA doit etre inversible"""
+    A, B = AA.copy(), BB.copy()
+    n = len(A)
+    assert len(A[0]) == n
+    # Mise sous forme triangulaire
+    for i in range(n):
+        j = recherche_pivot(A, i)
+        if j > i:
+            echange_lignes(A, i, j)
+            echange_lignes(B, i, j)
+        for k in range(i+1, n):
+            x = A[k][i] / float(A[i][i])
+            transvection_ligne(A, k, i, -x)
+            transvection_ligne(B, k, i, -x)
+    # Phase de remontée
+    X = [0.] * n
+    for i in range(n-1, -1, -1):
+        X[i] = (B[i][0]-sum(A[i][j]*X[j] for j in range(i+1,n))) / A[i][i]
+    return X
+
+
+
+AA = [[-1,-2,-3],[0,-1,-4],[-3,-4,-1]]
+BB = [[1],[3],[1]]
+       
+res1 = resolution_gauss_jordan(AA,BB)
+res2 = np.linalg.solve(AA,BB)
+print(res1)
+print(res2)
+
+
+
+
 
 # ====================
 # Algorithmes de tris
