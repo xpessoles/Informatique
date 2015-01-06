7b4686f0b06642518c8b093c527e6130a5aa8a34
Xavier Pessoles <xpessoles.ptsi@free.fr>
2015-01-06 15:55:07 +0100

    MAJ

--- TP/06_AnalyseMesure/programme/Corrige_TraitementMesure.py
+++ TP/06_AnalyseMesure/programme/Corrige_TraitementMesure.py
@@ -134,11 +134,64 @@ def filtrageMoyenneGlissante(fichier,nbEch):
     print("Affichage")
     plt.plot(t2,mal2)
     plt.show()
+    
+def filtrageKalman(fichier) :
+    # Kalman filter example demo in Python
+
+    # A Python implementation of the example given in pages 11-15 of "An
+    # Introduction to the Kalman Filter" by Greg Welch and Gary Bishop,
+    # University of North Carolina at Chapel Hill, Department of Computer
+    # Science, TR 95-041,
+    # http://www.cs.unc.edu/~welch/kalman/kalmanIntro.html
+
+    # by Andrew D. Straw
+    import numpy
+    import pylab
+    t,man,mal = lireFichierMesure_04("MesureCapsuleuse.lvm")
+    z=mal
+    # intial parameters
+    n_iter = len(z)
+    sz = (n_iter,) # size of array
+    x = -0.37727 # truth value (typo in example at top of p. 13 calls this z)
+    #z = numpy.random.normal(x,0.1,size=sz) # observations (normal about x, sigma=0.1)
+    
+    Q = 1e-5 # process variance
+    
+    # allocate space for arrays
+    xhat=numpy.zeros(sz)      # a posteri estimate of x
+    P=numpy.zeros(sz)         # a posteri error estimate
+    xhatminus=numpy.zeros(sz) # a priori estimate of x
+    Pminus=numpy.zeros(sz)    # a priori error estimate
+    K=numpy.zeros(sz)         # gain or blending factor
+    
+    R = 0.1**2 # estimate of measurement variance, change to see effect intial guesses
+    xhat[0] = 0.0
+     
+    P[0] = 1.0
+    for k in range(1,n_iter):
+        # time update
+        xhatminus[k] = xhat[k-1]
+        Pminus[k] = P[k-1]+Q
+        
+        # measurement update
+        K[k] = Pminus[k]/( Pminus[k]+R )
+        xhat[k] = xhatminus[k]+K[k]*(z[k]-xhatminus[k])
+        P[k] = (1-K[k])*Pminus[k]
+
+    pylab.figure()
+    pylab.plot(xhat,'b-',label='a posteri estimate')
+    pylab.legend()
+    pylab.xlabel('Iteration')
+    pylab.ylabel('Voltage')
+    
+    pylab.show()
+    
 
 fichier = "MesureCapsuleuse.lvm"
 
 # plotFichier(fichier)
-filtrageMoyenneGlissante(fichier,20)
+filtrageMoyenneGlissante(fichier,1000)
+#plotFichier(fichier)
 
 
 input("Tapper une touche pour terminer")
