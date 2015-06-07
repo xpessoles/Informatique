d8717d7a005ab51121ab8a690047a1a22917c3fc
Xavier Pessoles <xpessoles.ptsi@free.fr>
2015-06-07 22:55:03 +0200

    Code

---
+++ Projets/01_ModelisationAmortisseur/Programme/biblio.py
@@ -0,0 +1,25 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+__author__      = "Xavier Pessoles"
+__email__ = "xpessoles.ptsi@free.fr"
+
+def solve_eq(m,k,c,x0,v0,h,T):
+    """
+    Résolution de l'équation différentielle du second ordre pour un système
+    masse amortisseur.
+    Entrées : 
+     * m,flt : masse en kg
+     * k,flt : raideur du ressort en N/m
+     * c,flt : coefficient d'amortissement en N.s/m
+     * x0,flt : position initiale en m
+     * v0,flt : vitesse initiale en m/s
+     * h,flt : pas de temps de calcul
+     * T,flt ; temps de simulation
+    Sortie : 
+     * 
+    
+    """
+    res=[x0,v0]
+    t=h
+    while t<T:
+        res.append((1/(m+k*h*h+c*h))*(h*h*f(t)+res[-1]*(2*m+c*h)-m*res[-2]))
---
+++ Projets/01_ModelisationAmortisseur/Programme/main.py
@@ -0,0 +1,17 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+__author__      = "Xavier Pessoles"
+__email__ = "xpessoles.ptsi@free.fr"
+
+
+# Raideur du ressort en N/m
+k = 1000000
+# Coefficient d'amortissement en N.s/m
+c = 50
+# Masse en kg
+m = 1000
+# Pas de temps en seconde
+h = 0.001
+# Temps total en s
+T = 1
+
