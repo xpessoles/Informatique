be9e15b7286a78ac7a61cb7dcc84247aefe5643d
Xavier Pessoles <xpessoles.ptsi@free.fr>
2015-11-08 22:53:18 +0100

    MAJ

--- P_05_AlgorithmiqueProgrammation/02_Piles/TD_02/programmes/TD_02_XP.py
+++ P_05_AlgorithmiqueProgrammation/02_Piles/TD_02/programmes/TD_02_XP.py
@@ -4,11 +4,7 @@
 # Exercice 1 : Construction d'une pile de longueur finie
 # ======================================================
 def creer_pile(n):
-    assert n>=2
-    pile = (None,None)
-    for i in range(2,n):
-        pile=pile+(None,)
-    return pile
+    return n*[None]
     
 def est_vide(pile):
     for el in pile :
@@ -19,57 +15,70 @@ def est_vide(pile):
 def est_pleine(pile):
     return (not None in pile) 
 
-def depiler(pile):
-    # On recherche l'indice du premier élément None de la pile
-    if est_pleine(pile):
-        i = len(pile)
-    else :
-        i=0
-        while pile[i] != None:
-            i=i+1
-    el = pile[i-1]
-    pile2 = creer_pile(len(pile))
-    for j in range(i-1):
-        pile2 = empiler(pile2,pile[j])
-    pile = creer_pile(len(pile))
-    for i in range(len(pile)):
-        pile = empiler(pile,pile2[i])
-    return pile,el
-
 def empiler(pile,el):
-    n=len(pile)
-    taille = len(pile)
     if est_pleine(pile):
-        # Si la pile est pleine on ne fait rien
-        return pile
-    else :
-        if est_vide(pile):
-            pile = (el,None)+creer_pile(n-2)
-        elif pile[1] == None :
-            pile = (pile[0],el)+creer_pile(n-2)
-        else :
-            #On recherche l'indice du premier élément None de la pile
-            i=0
-            while pile[i] != None:
-                i=i+1
-            if i == n:
-                print(pile)
-                return pile
-            pile=pile[0:i]+(el,)
-            if n-i == 2:
-                pile = pile+(None,)
-            elif n-i>1:
-                pile=pile+creer_pile(n-i)
-    return pile    
+        return None
+    # On recherche le premier emplacement vide
+    i=0
+    while pile[i]!= None:
+        i=i+1
+    pile[i]=el
+
+def depiler(pile):
+    if est_vide(pile):
+        return None
+    # On recherche le premier emplacement vide
+    if est_pleine(pile):
+        l = len(pile)
+        el = pile[l-1]
+        pile[l-1]=None
+        return el
+    i=0
+    while pile[i]!= None:
+        i=i+1
+    el = pile[i-1]
+    pile[i-1]=None
+    return el    
     
-n=4
-pile = creer_pile(n)
-pile = empiler(pile,1)
-pile = empiler(pile,2)
-pile = empiler(pile,3)
-pile = empiler(pile,4)
-pile = empiler(pile,5)
-pile = empiler(pile,6)
-pile = empiler(pile,7)
-pile,el=depiler(pile)
-#print(pile)
+def taille_pile(pile):
+    return len(pile)
+# Exercice 2 : NPI
+# ================
+def est_nombre(el):
+    return type(el)==float or type(el)==int
+
+def est_operation(el):
+    return el in ["+","-","*","/"]
+    
+def inverse(pile):
+    pile2=creer_pile(taille_pile(pile))
+    pile3=creer_pile(taille_pile(pile))
+    while not (est_vide(pile)):
+        empiler(pile2,depiler(pile))
+    while not (est_vide(pile2)):
+        empiler(pile3,depiler(pile2))
+    while not (est_vide(pile3)):
+        empiler(pile,depiler(pile3))
+
+def operer(nb1,nb2,op):
+    if op == "+":
+        return nb1+nb2
+    elif op == "*":
+        return nb1*nb2
+        
+def evaluer(pile):
+    inverse(pile)
+    pile2=creer_pile(taille_pile(pile))
+    while not est_vide(pile):
+        el = depiler(pile)
+        if est_nombre(el):
+            empiler(pile2,el)
+        elif est_operation(el) :
+            empiler(pile2,operer(depiler(pile2),depiler(pile2),el))
+    return depiler(pile2)
+            
+    
+    
+pile1=[1,2,"+",4,"*",3,"+"]
+pile2=[1,2,"+",4,"*",-3,"+",5,"+"]
+print(evaluer(pile2))
