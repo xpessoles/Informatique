7b474c4d427ae1000e28a976a8dc181feacae8d8
Xavier Pessoles <xpessoles.ptsi@free.fr>
2016-04-15 14:27:37 +0200

    Import initial

---
+++ PreparationOraux/Exercices_Maths/programmes/Exercice_03/exercice_03.py
@@ -0,0 +1,31 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+__author__ = "Xavier PESSOLES"
+
+# Import des bibliothèques
+import matplotlib.pyplot as plt
+
+# Question 1
+# ==========
+# Matrices avec des listes
+M=[[0,9,3,-1,7],[9,0,1,8,-1],[3,1,0,4,2],[-1,8,4,0,-1],[7,-1,2,-1,0]]
+
+# Question 2 & 3
+# ==============
+def voisins(M,i):
+    """
+    Entrées : 
+      * M(lst) : graphe
+      * i : noeud considéré
+    """
+    v = []
+    for j in range(len(M[i])):
+        if M[i][j]>0:
+            v.append(j)
+    return v
+    
+print(voisins(M,0))
+print(voisins(M,1))
+print(voisins(M,2))
+print(voisins(M,3))
+print(voisins(M,4))
