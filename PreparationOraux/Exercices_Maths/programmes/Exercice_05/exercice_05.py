4af75045fc7c2fbceca2b48d12d3f72c84c17914
Xavier Pessoles <xpessoles.ptsi@free.fr>
2016-04-17 16:51:34 +0200

    Ajout de corrigé

---
+++ PreparationOraux/Exercices_Maths/programmes/Exercice_05/exercice_05.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+__author__ = "Xavier PESSOLES"
+
+# EXERCICE 5
+
+# Question 1 
+# ==========
+
+
+# Question 2
+# ==========
+
+
+# Question 3
+# ==========
