0253ab8200a2bc4d04edd6fdd6864947f2fcfa1a
Xavier Pessoles <xpessoles.ptsi@free.fr>
2016-04-18 22:16:02 +0200

    Import initial exo 10

---
+++ PreparationOraux/Exercices_Maths/programmes/Exercice_10/exercice_10.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+__author__ = "Xavier PESSOLES"
+
+# EXERCICE 10
+import numpy as np
+
+# Question 1
+# ==========
+R = np.array([[1,2,3],[4,5,6]])
+S = np.array([[1,2,3],[4,5,6],[7,8,9]])
+
+# Question 2
+# ==========
