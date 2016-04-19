c3919d6a9337b07923afb052b950d62636d87ec0
Xavier Pessoles <xpessoles.ptsi@free.fr>
2016-04-19 11:06:00 +0200

    Exo 11

---
+++ PreparationOraux/Exercices_Maths/programmes/Exercice_11/exercice_11.py
@@ -0,0 +1,63 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+__author__ = "Xavier PESSOLES"
+
+# EXERCICE 11
+
+# Question 1 
+# ==========
+def comptage(L,n):
+    """
+    Comptage des éléments de L.
+    Entrées :
+     * n(int) : entier
+     * L(lst) : liste d'éléments inférieurs à n
+    """
+    P = [0 for i in range(n+1)]
+    # P = [0]*(n+1)
+    for e in L:
+        P[e]=P[e]+1
+    return P
+from random import randint
+maxi = 5
+LL = [randint(0,maxi) for x in range(20)]
+P = comptage(LL,maxi)
+# print(LL)
+# print(P)
+    
+# Question 2
+# ==========
+def tri(L,n):
+    """
+    Tri une liste.
+    Entrées :
+     * n(int) : entier
+     * L(lst) : liste d'éléments inférieurs à n
+    Sortie : 
+     * T(lst) : liste triée.
+    """
+    P = comptage(L,n)
+    T = []
+    for i in range(len(P)):
+        for j in range(P[i]):
+            T.append(i)
+    return T
+
+# Question 3
+# ==========
+from random import randint
+maxi = 5
+LL = [randint(0,maxi) for x in range(20)]
+T = tri(LL,maxi)
+print(LL)
+print(T)
+
+
+# Question 4
+# ==========
+# Complexité quadratique : C(n)=O(n+n²)=0(n²)
+# n : complexité de comptage
+# n² : complexité des deux boucles imbriquées du tri
+# Ce tri s'exéctuera toujours dans le pire des cas.
+# Dans le cas moyen : tri fusion O(nlogn)
+# Dans le cas moyen : tri insertion O(n²)
