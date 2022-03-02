bd42425cdfa82cb9a98e8fec37004e1c3437c53d
Xavier Pessoles <xpessoles.ptsi@free.fr>
2022-03-02 11:16:17 +0100

    MAJ

--- S2_Cours/03_PreuvesAlgorithmes/03_PreuvesAlgorithmes.py
+++ S2_Cours/03_PreuvesAlgorithmes/03_PreuvesAlgorithmes.py
@@ -12,10 +12,19 @@ def foo2(v:int) -> int:
     r = 1
     n = 1
     while r < v : 
-        r = r+n
         n = n+1
-        
+        r = r+n
+        print(r,n,v)
     return n,sum([i for i in range(n+1)])
 
-# foo(10) renvoie 4
-res = 1+2+3+4
+def foo3(v:int) -> int:
+    r = 0
+    n = 0
+    while r <= v : 
+        n = n+1
+        r = r+n
+        print(r,n,v)
+    return n,sum([i for i in range(n+1)])
+
+
+
