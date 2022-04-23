91a15ccf9ac3e500d0c83e6bfcf14d5e2d17b161
Xavier Pessoles <xpessoles.ptsi@free.fr>
2022-04-23 15:49:23 +0200

    MAJ

---
+++ Correction_Pytest/Correction_Auto.py
@@ -0,0 +1,11 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sat Apr 23 15:29:40 2022
+
+@author: Xavier Pessoles
+"""
+
+# Répetoire contenant les scripts des élèves
+REP_ELEVE = "scripts_eleves" 
+
+# Répertoire de correction
