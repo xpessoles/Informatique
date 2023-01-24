171b965170ff5a27ad86ae8564d020d1f30ba41f
Xavier Pessoles <xpessoles.ptsi@free.fr>
2023-01-24 22:24:40 +0100

    MAJ

--- Exercices/S1_06_Gloutons/06_RechercheChemin/06_RechercheChemin_eleve_2022.py
+++ Exercices/S1_06_Gloutons/06_RechercheChemin/06_RechercheChemin_eleve_2022.py
@@ -9,25 +9,25 @@ n=10
 
 
 
-# Question 4
+# Question 1
 ############
 def distance(p1:list, p2:list) -> float:
     # A compléter    
     return 0.
 
-def Q4_test():
+def Q1_test():
     """ Fonction de test pour la question 4."""
     A, p1, p2 = [0,0], [2,4], [6,5]
     return (distance(A,p1) > 4.472135) and (distance(p1,p2) < 4.472136)
  
     
-# Question 5
+# Question 2
 ############
 def distances(pts, dep):
     # A compléter    
     return []
 
-def Q5_test():
+def Q2_test():
     """ Fonction de test pour la question 5."""
     A, p1, p2, p3 = [0,0], [2,4], [6,5], [5,1]
     pts = [p1,p2,p3]
@@ -35,17 +35,17 @@ def Q5_test():
     tab_dist = [[0, 4.123105625617661, 4.242640687119285, 4.47213595499958], [4.123105625617661, 0, 4.123105625617661, 7.810249675906654], [4.242640687119285, 4.123105625617661, 0, 5.0990195135927845], [4.47213595499958, 7.810249675906654, 5.0990195135927845, 0]]
     return np.allclose(np.array(tab_dist),np.array(dist))
 
-# Question 6
+# Question 3
 ############
-ques_2 = 0  # Modifier le 0 par votre réponse (en fonction de n)
+ques_3 = 0  # Modifier le 0 par votre réponse (en fonction de n)
 
-# Question 7
+# Question 4
 ############
 def longueur(chemin, dist):
     # A compléter    
     return 0.
 
-def Q7_test():
+def Q4_test():
     """ Fonction de test pour la question 7."""
     chemin = [3,2,1,0]
     tab_dist = [[0, 4.123105625617661, 4.242640687119285, 4.47213595499958], [4.123105625617661, 0, 4.123105625617661, 7.810249675906654], [4.242640687119285, 4.123105625617661, 0, 5.0990195135927845], [4.47213595499958, 7.810249675906654, 5.0990195135927845, 0]]
@@ -53,50 +53,50 @@ def Q7_test():
     return (d > 13.34523076) and (d < 13.34523077)
     
 
-# Question 8
+# Question 5
 ############
 def longueur_rec(chemin, dist):
     # A compléter    
     return 0.
 
-# Question 9
+# Question 6
 ############
-ques_9_pts  = 0 # A compléter
-ques_9_pts_parc  = 0 # A compléter
-ques_9_pts_parc_liste = "" # Chaine de caractères sous la forme "a,p1,p2,p3"
+ques_6_pts  = 0 # A compléter
+ques_6_pts_parc  = 0 # A compléter
+ques_6_pts_parc_liste = "" # Chaine de caractères sous la forme "a,p1,p2,p3"
 
 
-# Question 10
+# Question 7
 #############
-ques_10  = "" # A compléter
+ques_7  = "" # A compléter
 
 
-# Question 11
+# Question 8
 #############
 def indice(position, dist, dispo):
     # A compléter    
     return 0
 
-def Q11_test():
+def Q8_test():
     """ Fonction de test pour la question 11."""
     dispo = [False, True, True, False]
     tab_dist = [[0, 4.123105625617661, 4.242640687119285, 4.47213595499958], [4.123105625617661, 0, 4.123105625617661, 7.810249675906654], [4.242640687119285, 4.123105625617661, 0, 5.0990195135927845], [4.47213595499958, 7.810249675906654, 5.0990195135927845, 0]]
     return 1 == indice(0, tab_dist, dispo)
     
-# Question 12
+# Question 9
 #############
 def plus_court(dist):
     # A compléter    
     return []
 
-def Q12_test():
+def Q9_test():
     """ Fonction de test pour la question 12."""
     tab_dist = [[0, 4.123105625617661, 4.242640687119285, 4.47213595499958], [4.123105625617661, 0, 4.123105625617661, 7.810249675906654], [4.242640687119285, 4.123105625617661, 0, 5.0990195135927845], [4.47213595499958, 7.810249675906654, 5.0990195135927845, 0]]
     return  plus_court(tab_dist) == [0, 1, 2]
 
 
 
-# Question 13
+# Question 10
 #############
 
 def plot_chemin():
