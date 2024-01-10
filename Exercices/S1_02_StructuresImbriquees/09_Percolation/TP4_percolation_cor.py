a25e894289573106f5a0cec48d35577ca99a8780
Xavier Pessoles <xpessoles.ptsi@free.fr>
2024-01-10 11:14:38 +0100

    Update TP4_percolation_cor.py

--- Exercices/S1_02_StructuresImbriquees/09_Percolation/TP4_percolation_cor.py
+++ Exercices/S1_02_StructuresImbriquees/09_Percolation/TP4_percolation_cor.py
@@ -126,7 +126,7 @@ def a_un_voisin_rempli(grille: list,i:int,j:int):
     '''renvoie un booléen indiquant si l'un des voisins est rempli (valeur 2)'''
     test=False
     n=len(grille)
-    if j+1<n and grille[i][j+1]==2:
+    if j+1<n and grille[i][j+1]==2: 
         test=True
     elif j-1>=0 and grille[i][j-1]==2 :
         test=True
