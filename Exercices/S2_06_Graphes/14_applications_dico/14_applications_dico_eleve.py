894c846fa27ab5f0a1b0be175772e8717575b6a6
Xavier Pessoles <xpessoles.ptsi@free.fr>
2026-04-28 21:08:09 +0200

    MAJ

---
+++ Exercices/S2_06_Graphes/14_applications_dico/14_applications_dico_eleve.py
@@ -0,0 +1,26 @@
+## Question 1
+dico_test = {
+    19 : "Corrèze",
+    64 : "Pyrénées-Atlantiques",
+    40 : "Landes",
+    33 : "Gironde",
+    47 : "Lot-et-Garonne",
+    16 : "Charente",
+    17 : "Charente-Maritime",
+    24 : "Dordogne",
+    79 : "Deux-Sèvres",
+    86 : "Vienne",
+    87 : "Haute-Vienne",
+    }
+
+## Question 2
+aqui_test = {
+            16:[17,24,79,86,87],
+            17:[16,24,79],
+            19:[23,24,87],
+            23:[87,19],
+            24:[17,16,87,19],
+            79:[86,16,17],
+            86:[79,16,87],
+            87:[23,19,24,16,86]}
+
